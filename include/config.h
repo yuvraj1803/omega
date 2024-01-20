774272630668909c31f395ac9c16b503acfd088d
yuvraj1803 <ysakshith@gmail.com>
2024-01-20 16:55:02 +0530

    added SCR_EL3 vals into config.h

--- include/config.h
+++ include/config.h
@@ -8,14 +8,20 @@
 #ifndef __config_h__
 #define __config_h__
 
+// #define DEBUG
+
 #define BIT(x)                          ((1U << x))
 
 #define EL3_SCTLR_M                     BIT(0)
 #define EL3_SCTLR_WXN                   BIT(19)
-
-// #define DEBUG
-
 #define EL3_SCTLR_NO_MMU                EL3_SCTLR_WXN                
 #define EL3_SCTLR_WITH_MMU              EL3_SCTLR_NO_MMU | EL3_SCTLR_M
+
+
+#define EL3_SCR_NS                      BIT(0)
+#define EL3_SCR_HCE                     BIT(8)
+#define EL3_SCR_RW                      BIT(10)
+#define EL3_SCR                         EL3_SCR_NS | EL3_SCR_HCE | EL3_SCR_RW
+
 
 #endif
