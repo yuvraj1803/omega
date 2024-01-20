baeaef29587c64d72911b5900ff59974f8ac7606
yuvraj1803 <ysakshith@gmail.com>
2024-01-20 13:57:48 +0530

    added BIT(x) macro

--- include/config.h
+++ include/config.h
@@ -8,10 +8,13 @@
 #ifndef __config_h__
 #define __config_h__
 
+#define BIT(x)  ((1U << x))
+
+#define SCTLR_EL3_M     BIT(0)
 
 // #define DEBUG
 
-#define EL3_SCTLR_WITH_MMU  /*M*/   (1 << 0)
+#define EL3_SCTLR_WITH_MMU              SCTLR_EL3_M
 #define EL3_SCTLR_NO_MMU                0
 
 #endif
