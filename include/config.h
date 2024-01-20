ec47bc3928a4253fcae9d5c6888c714241635aca
yuvraj1803 <ysakshith@gmail.com>
2024-01-20 16:35:35 +0530

    added SCTLR_WXN bit to EL3 config.

--- include/config.h
+++ include/config.h
@@ -8,13 +8,14 @@
 #ifndef __config_h__
 #define __config_h__
 
-#define BIT(x)  ((1U << x))
+#define BIT(x)                          ((1U << x))
 
-#define SCTLR_EL3_M     BIT(0)
+#define EL3_SCTLR_M                     BIT(0)
+#define EL3_SCTLR_WXN                   BIT(19)
 
 // #define DEBUG
 
-#define EL3_SCTLR_WITH_MMU              SCTLR_EL3_M
-#define EL3_SCTLR_NO_MMU                0
+#define EL3_SCTLR_NO_MMU                EL3_SCTLR_WXN                
+#define EL3_SCTLR_WITH_MMU              EL3_SCTLR_NO_MMU | EL3_SCTLR_M
 
 #endif
