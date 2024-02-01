82aae8a217d405cc7b9b2049cbfa3fb193376c87
yuvraj1803 <ysakshith@gmail.com>
2024-02-01 10:52:20 +0530

    added emmc driver

---
+++ include/drivers/sd.h
@@ -0,0 +1,20 @@
+/*
+ *
+ * Copyright (c) 2023 Yuvraj Sakshith <ysakshith@gmail.com>
+ *
+ */
+
+
+#ifndef __sd_h__
+#define __sd_h__
+
+#define SD_OK                0
+#define SD_TIMEOUT          -1
+#define SD_ERROR            -2
+
+int sd_init();
+int sd_readblock(unsigned int lba, unsigned char *buffer, unsigned int num);
+int sd_writeblock(unsigned int lba, unsigned char* buffer, unsigned int num);
+
+
+#endif
---
+++ include/monitor/delay.h
@@ -0,0 +1,14 @@
+/*
+ *
+ * Copyright (c) 2023 Yuvraj Sakshith <ysakshith@gmail.com>
+ *
+ */
+
+
+#ifndef __delay_h__
+#define __delay_h__
+
+void delay_cycles(int nr_cycles);
+void delay_micro_sec(int nr);
+
+#endif
