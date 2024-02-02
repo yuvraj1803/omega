05dd37054fc4e535e3b9357762114e952e970107
yuvraj1803 <ysakshith@gmail.com>
2024-02-02 10:41:36 +0530

    added EL3_SPSR_EL2h

--- include/config.h
+++ include/config.h
@@ -18,11 +18,13 @@
 #define EL3_SCTLR_NO_MMU                EL3_SCTLR_WXN                
 #define EL3_SCTLR_WITH_MMU              EL3_SCTLR_NO_MMU | EL3_SCTLR_M
 
-
 #define EL3_SCR_NS                      BIT(0)
 #define EL3_SCR_HCE                     BIT(8)
 #define EL3_SCR_RW                      BIT(10)
 #define EL3_SCR                         EL3_SCR_NS | EL3_SCR_HCE | EL3_SCR_RW
 
+#define EL3_SPSR_DAIF_MASK              (BIT(9) | BIT(8) | BIT(7) | BIT(6))
+#define EL3_SPSR_M_EL2h                 (0b1001)
+#define EL3_SPSR_EL2h                   EL3_SPSR_DAIF_MASK | EL3_SPSR_M_EL2h
 
 #endif
