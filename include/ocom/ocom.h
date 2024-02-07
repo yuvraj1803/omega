4e1fefb35c5c710d8104aac269c64b7290dd7391
yuvraj1803 <ysakshith@gmail.com>
2024-02-07 17:02:16 +0530

    added ocom_store and ocom_load function skeletons.

--- include/ocom/ocom.h
+++ include/ocom/ocom.h
@@ -6,4 +6,14 @@
 
 #ifndef __OCOM_H__
 #define __OCOM_H__
+
+#define OCOM_STORE_SMC_NR   0
+#define OCOM_LOAD_SMC_NR    1
+
+#include <stdint.h>
+
+int ocom_load_page(uint64_t key, uint64_t dest_virt);
+int ocom_store_page(uint64_t key, uint64_t src_virt);
+
+
 #endif
--- ocom/ocom.c
+++ ocom/ocom.c
@@ -4,6 +4,17 @@
  *
  */
 
-#include "ocom/ocom.h
+#include "ocom/ocom.h"
 
-"
+
+// the monitor will take the mapping of key and place the page in the destination virtual address provided.
+int ocom_load_page(uint64_t key, uint64_t dest_virt){
+
+}
+
+
+// the monitor will take the contents from the virtual address provided of the page.
+// this contents will be store in the NAS.
+int ocom_store_page(uint64_t key, uint64_t src_virt){
+
+}
