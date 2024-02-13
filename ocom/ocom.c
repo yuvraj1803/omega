b5c295ccae431bfd6189166fea062a42417520a1
yuvraj1803 <ysakshith@gmail.com>
2024-02-13 18:45:15 +0530

    added virt to phys conversion in ocom handler on monitor side. Also checks have been placed to identify invalid requests.

--- ocom/ocom.c
+++ ocom/ocom.c
@@ -5,10 +5,24 @@
  */
 
 #include "ocom/ocom.h"
-
+#include "mm/paging.h" 
+#include "mm/mm.h"
+#include "memory.h"
+#include "stdio.h"
 
 // the monitor will take the mapping of key and place the page in the destination virtual address provided.
 int ocom_load_page(uint64_t key, uint64_t dest_virt){
+
+    uint64_t dest_phys = paging_va_to_pa(dest_virt); // this is the physical address of the page which the VM provided us.
+    uint8_t data_plain[PAGE_SIZE];
+    
+    if(dest_phys == 0){
+        printf("Invalid page provided. Does not exist in IPA.\n");
+        return -1;
+    }
+
+
+    
 
     return 0;
 }
