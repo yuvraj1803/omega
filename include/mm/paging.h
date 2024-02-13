492743827e56ce488f4e5c9f5e4fa108063ae6b6
yuvraj1803 <ysakshith@gmail.com>
2024-02-13 14:56:46 +0530

    added get_vttbr_el2()

---
+++ include/mm/paging.h
@@ -0,0 +1,15 @@
+
+/*
+ *
+ * Copyright (c) 2024 Yuvraj Sakshith <ysakshith@gmail.com>
+ *
+ */
+
+#ifndef __PAGING_H__
+#define __PAGING_H__
+
+#include <stdint.h>
+
+uint64_t get_vttbr_el2();
+
+#endif
